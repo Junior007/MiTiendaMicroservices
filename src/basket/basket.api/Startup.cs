b2f2c591b5f0182836566c1b6fcd8a69ccd4a079
Junior007 <agilgomez@gmail.com>
2021-02-18 17:15:24 +0100

    Empiezo la cesta de la compra

--- src/basket/basket.api/Startup.cs
+++ src/basket/basket.api/Startup.cs
@@ -1,17 +1,17 @@
-using catalog.api.settings;
-using catalog.data.context;
-using catalog.data.interfaces;
-using catalog.infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-namespace catalog.api
+namespace basket.api
 {
     public class Startup
     {
@@ -25,33 +25,16 @@ namespace catalog.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "catalog.api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "basket.api", Version = "v1" });
             });
             services.AddMvc().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
 
-
-
-            RegisterServices(services);
-
-
-        }
-        //
-        private void RegisterServices(IServiceCollection services)
-        {
-
-            //Para inyectar las propiedades de la conexión en el contexto
-            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
-            services.AddSingleton<ICatalogDatabaseSettings>(
-                sp => sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
-            services.AddTransient<ICatalogContext, CatalogContext>();
-
-            DependencyContainer.RegisterServices(services);
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,11 +44,8 @@ namespace catalog.api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "catalog.api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "basket.api v1"));
             }
-            //Configuration.GetConnectionString("BankingDbConnection")
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
