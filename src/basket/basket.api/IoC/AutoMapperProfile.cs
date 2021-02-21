33c8d37cf846f30893e9bc78a7740ef99f42b0e6
Junior007 <agilgomez@gmail.com>
2021-02-21 14:14:18 +0100

    Testeado basket.api, ok. Falta checkout

--- src/basket/basket.api/IoC/AutoMapperProfile.cs
+++ src/basket/basket.api/IoC/AutoMapperProfile.cs
@@ -21,10 +21,18 @@ namespace basket.IoC
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculatedAge()));
 
             CreateMap<Photo, PhotoForDetailed>();
-            CreateMap<Photo, PhotoForReturn>();
+            CreateMap<Photo, PhotoForReturn>();*/
+
+            //OUT
+            CreateMap<basket.domain.models.BasketCart,     basket.application.models.BasketCart>();
+            CreateMap<basket.domain.models.BasketCartItem, basket.application.models.BasketCartItem>();
+            CreateMap<basket.domain.models.BasketCheckout, basket.application.models.BasketCheckout>();
+
+
             // IN
-            CreateMap<UserForUpdate, User>();
-            CreateMap<PhotoForCreation, Photo>();*/
+            CreateMap<basket.application.models.BasketCart, basket.domain.models.BasketCart>();
+            CreateMap<basket.application.models.BasketCartItem, basket.domain.models.BasketCartItem>();
+            CreateMap<basket.application.models.BasketCheckout, basket.domain.models.BasketCheckout>();
 
         }
     }
